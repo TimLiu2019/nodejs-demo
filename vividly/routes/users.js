1b45730246ee08d9e9da739fd12cd593e58fc7dc
TimLiu2019 <qitingliu7@gmail.com>
2021-04-21 14:48:28 -0400

    use lodash to pick user properties

--- vividly/routes/users.js
+++ vividly/routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Joi = require("joi");
+const _ = require("lodash");
 const { User, validate } = require("../models/user");
 
 router.get("/", async (req, res) => {
@@ -15,13 +16,15 @@ router.post("/", async (req, res) => {
 
   let user = await User.findOne({ email: req.body.emal });
   if (user) return res.status(400).send("User already registered.");
-   user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  });
+  //    user = new User({
+  //     name: req.body.name,
+  //     email: req.body.email,
+  //     password: req.body.password
+  //   });
+  // or use pick
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
   user = await user.save();
-  res.send(user);
+  res.send(_.pick(user, ["_id", "name", "email"]));
 });
 
 module.exports = router;
