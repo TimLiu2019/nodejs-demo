41c8529a5b3d821d3d315fe708a963bcd53c8be9
TimLiu2019 <qitingliu7@gmail.com>
2021-04-24 21:30:12 -0400

    express async errors

--- vividly/routes/genres.js
+++ vividly/routes/genres.js
@@ -1,30 +1,28 @@
+const asyncMiddleware = require('../middleware/async');
 const express = require("express");
 const router = express.Router();
 const Joi = require("joi");
-const auth = require('../middleware/auth');
-const admin = require('../middleware/admin');
+const auth = require("../middleware/auth");
+const admin = require("../middleware/admin");
 const { Genre, validate } = require("../models/genre");
 
-router.get("/", async (req, res, next) => {
-  try {
+router.get(
+  "/",
+  asyncMiddleware(async (req, res) => {
     const genres = await Genre.find().sort("name");
-  res.send(genres);
-  } catch(ex){
-    // log exception
-    next(ex);
-  }
-  
-});
-router.get("/:id", async (req, res) => {
+    res.send(genres);
+  })
+);
+router.get("/:id", asyncMiddleware( async (req, res) => {
   const genre = await Genre.findByIdAndUpdate(req.params.id);
   //  const genre = genres.find(g => g.id === parseInt(req.params.id));
   if (!genre)
     return res.status(404).send("The genre with given id was not found");
 
   res.send(genre);
-});
+}));
 
-router.post("/", auth,async (req, res) => {
+router.post("/", auth, async (req, res) => {
   const result = validate(req.body);
   if (result.error) return res.status(404).send(result.error);
   console.log(result.error);
@@ -55,7 +53,7 @@ router.put("/:id", async (req, res) => {
   res.send(genre);
 });
 
-router.delete("/:id",[auth, admin],async (req, res) => {
+router.delete("/:id", [auth, admin], async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
   // look up if this genre exist
   // if not 404
