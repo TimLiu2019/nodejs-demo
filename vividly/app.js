4d09dcafa3429e32d457daa10bda7989badd3423
TimLiu2019 <qitingliu7@gmail.com>
2021-04-27 15:55:37 -0400

    extracting the logging logic

--- vividly/app.js
+++ vividly/app.js
@@ -1,15 +1,16 @@
 require("express-async-errors");
 const winston = require("winston");
 require("winston-mongodb");
-const mongoose = require("mongoose");
 const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const express = require("express");
 const app = express();
 
+require("./startup/logging");
 require("./startup/routes")(app);
 require("./startup/db")();
+
 
 // process.on('uncaughtException',ex =>{
 //   console.log('We got an uncaught exception');
@@ -17,26 +18,9 @@ require("./startup/db")();
 //   process.exit(1);
 // });
 
-winston.handleExceptions(
-  new winston.transports.File({ filename: "uncaughtExceptions.log" })
-);
-process.on("unhandledRejection ", ex => {
-  console.log("We got an unhandled rejection");
-  winston.error(ex.message, ex);
-  process.exit(1);
-});
-
-winston.add(new winston.transports.File({ filename: "logfile.log" }));
-winston.add(
-  new winston.transports.MongoDB({
-    db: "mongodb://localhost/vividly",
-    level: "info"
-  })
-);
-
 //throw new Error("Something failed during startup");
-const p = Promise.reject(new Error("Something failed miseably!"));
-p.then(() => console.log("Done"));
+// const p = Promise.reject(new Error("Something failed miseably!"));
+// p.then(() => console.log("Done"));
 if (!config.get("jwtPrivateKey")) {
   console.error("jwtPrivateKey is not defined.");
   process.exit(1);
