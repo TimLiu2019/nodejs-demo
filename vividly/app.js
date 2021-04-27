6fd7931dfbcbc2a7bb18084ac462d1366b92c1a3
TimLiu2019 <qitingliu7@gmail.com>
2021-04-27 15:02:57 -0400

    extracting Routes

--- vividly/app.js
+++ vividly/app.js
@@ -1,27 +1,14 @@
 require("express-async-errors");
 const winston = require("winston");
 require("winston-mongodb");
-const error = require("./middleware/error");
 const mongoose = require("mongoose");
 const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const express = require("express");
 const app = express();
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
-app.use(express.json());
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-app.use(error);
+
+require('./startup/routes')(app);
 
 // process.on('uncaughtException',ex =>{
 //   console.log('We got an uncaught exception');
