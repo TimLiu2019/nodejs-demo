a492eab4d905a87a5f1ca61d2541bb7e378ff293
TimLiu2019 <qitingliu7@gmail.com>
2021-04-27 16:19:26 -0400

    extracting config logic

--- vividly/app.js
+++ vividly/app.js
@@ -1,7 +1,6 @@
 require("express-async-errors");
-const winston = require("winston");
 require("winston-mongodb");
-const config = require("config");
+
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const express = require("express");
@@ -10,7 +9,7 @@ const app = express();
 require("./startup/logging");
 require("./startup/routes")(app);
 require("./startup/db")();
-
+require("./startup/config");
 
 // process.on('uncaughtException',ex =>{
 //   console.log('We got an uncaught exception');
@@ -21,10 +20,6 @@ require("./startup/db")();
 //throw new Error("Something failed during startup");
 // const p = Promise.reject(new Error("Something failed miseably!"));
 // p.then(() => console.log("Done"));
-if (!config.get("jwtPrivateKey")) {
-  console.error("jwtPrivateKey is not defined.");
-  process.exit(1);
-}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(` Listening on port ${port}...`));
