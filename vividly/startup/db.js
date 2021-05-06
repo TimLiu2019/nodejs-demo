89d2fedbcc6de905ecb75f3580480a937cd244e9
TimLiu2019 <qitingliu7@gmail.com>
2021-05-06 19:42:53 -0400

    set up the test db

--- vividly/startup/db.js
+++ vividly/startup/db.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
 const logger = require("./logger");
+const config = require("config");
 module.exports = function() {
+  const db = config.get("db");
   mongoose
-    .connect("mongodb://localhost/vividly", {
+    .connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
       useCreateIndex: true
     })
-    .then(() => logger.info("Connected to Mongodb ..."))
-    .catch(() => console.error("Could not connect to mongodb"));
+    .then(() => logger.info(`Connected to ${db} ...`))
+    .catch(() => console.error(`Could not connect to ${db}`));
 };
