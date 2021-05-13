a29e82a294a7c822b2db108dd795980f5339b6ff
TimLiu2019 <qitingliu7@gmail.com>
2021-05-12 21:25:28 -0400

    unit test for User

--- vividly/tests/unit/models/user.test.js
+++ vividly/tests/unit/models/user.test.js
@@ -2,12 +2,15 @@ const { User } = require("../../../models/user");
 const jwt = require("jsonwebtoken");
 const config = require("config");
 const mongoose = require("mongoose");
-// describe("user.generateAuthToken", () => {
-//   it("should retrun a valid JWT", () => {
-//     const payload = { _id: new mongoose.Types.ObjectId().toHexStringuser, isAdmin: true };
-//     const user = new User(payload);
-//     const token = user.generateAuthToken();
-//     const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
-//     expect(decoded).toMatchObject(payload);
-//   });
-// });
+describe("user.generateAuthToken", () => {
+  it("should retrun a valid JWT", () => {
+    const payload = {
+      _id: new mongoose.Types.ObjectId().toHexString(), // otherwise,format is different
+      isAdmin: true
+    };
+    const user = new User(payload);
+    const token = user.generateAuthToken();
+    const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+    expect(decoded).toMatchObject(payload);
+  });
+});
