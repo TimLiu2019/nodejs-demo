2632c710b1f2fc10c11522cd78eaea2f75b5bc7c
TimLiu2019 <qitingliu7@gmail.com>
2021-05-17 19:17:11 -0400

    refactor gener test

--- vividly/tests/integration/genres.test.js
+++ vividly/tests/integration/genres.test.js
@@ -43,41 +43,45 @@ describe("api/genres", () => {
     });
   });
 
+  // define the happy path, in each test,
+  // change one para that clearly aligns with the name of the test
+  let token;
+  let name;
+
+  const exec = async () => {
+    return await request(server)
+      .post("/api/genres")
+      .set("x-auth-token", token)
+      .send({ name });
+  };
+
+  beforeEach(() => {
+    token = new User().generateAuthToken();
+    name = "genre1";
+  });
+
   describe("POST /", () => {
     it("should return 401 if client is not logged in", async () => {
-      const res = await request(server)
-        .post("/api/genres")
-        .send({ name: "genre1" });
+      token = "";
+      const res = await exec();
       expect(res.status).toBe(401);
     });
 
     it("should return 400 if genre is less than 5 characters", async () => {
-      const token = new User().generateAuthToken();
-      const res = await request(server)
-        .post("/api/genres")
-        .set("x-auth-token", token)
-        .send({ name: "1234" });
+      name = "1234";
+      const res = await exec();
       expect(res.status).toBe(400);
     });
 
     it("should return 400 if genre is more than 50 characters", async () => {
-      const token = new User().generateAuthToken();
-      const name = new Array(52).join("a");
+      name = new Array(52).join("a");
       console.log("50 characters", name, name.length);
-      const res = await request(server)
-        .post("/api/genres")
-        .set("x-auth-token", token)
-        .send({ name: name });
+      const res = await exec();
       expect(res.status).toBe(400);
     });
 
     it("should save the genre if it is valid", async () => {
-      const token = new User().generateAuthToken();
-
-      const res = await request(server)
-        .post("/api/genres")
-        .set("x-auth-token", token)
-        .send({ name: "genre1" });
+      const res = await exec();
 
       const genre = await Genre.find({ name: "genre1" });
       expect(genre).not.toBeNull();
