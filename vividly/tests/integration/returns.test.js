4f9a2effddcb4bc4979d1894490964d485cb6004
TimLiu2019 <qitingliu7@gmail.com>
2021-06-05 21:09:23 -0400

    testing the input

--- vividly/tests/integration/returns.test.js
+++ vividly/tests/integration/returns.test.js
@@ -1,15 +1,24 @@
 const request = require("supertest");
 const { Rental } = require("../../models/rental");
+const { User } = require("../../models/user");
 const mongoose = require("mongoose");
 describe("/api/returns", () => {
   let server;
   let customerId;
   let movieId;
   let rental;
+  let token;
+  const exec = () => {
+    return request(server)
+      .post("/api/returns")
+      .set("x-auth-token", token)
+      .send({ movieId, customerId });
+  };
   beforeEach(async () => {
     server = require("../../index");
     customerId = mongoose.Types.ObjectId();
     movieId = mongoose.Types.ObjectId();
+    token = new User().generateAuthToken();
     rental = new Rental({
       customer: {
         _id: customerId,
@@ -40,5 +49,22 @@ describe("/api/returns", () => {
       .send({ customerId, movieId });
 
     expect(res.status).toBe(401);
+  });
+
+  it("should should return 400 if customerId is not provided", async () => {
+    customerId = "";
+    const res = await exec();
+
+    expect(res.status).toBe(400);
+  });
+
+  it("should should return 400 if movieId is not provided", async () => {
+    const token = new User().generateAuthToken();
+    const res = await request(server)
+      .post("/api/returns")
+      .set("x-auth-token", token)
+      .send({ customerId });
+
+    expect(res.status).toBe(400);
   });
 });
